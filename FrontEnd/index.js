3894416fe2691201d403420300f50e06ed4891b3
Yace Sf <yacine.sf@outlook.fr>
2023-06-09 14:36:00 +0200

    ajout des filtres de façons dynamique

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,24 +1,58 @@
-class Article{
-  constructor(jsonArticle){
-    jsonArticle && Object.assign(this, jsonArticle)
+let categories = new Set();
+categories.add({ id: 0, name: "Tous" });
+let filters = document.querySelector(".filters");
+let articles = [];
+
+async function fetchWorks() {
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+    const data = await response.json();
+    articles = data;
+    for (let article of articles) {
+      document.querySelector(
+        ".gallery"
+      ).innerHTML += `<figure class="${article.categoryId}">
+                                                        <img src="${article.imageUrl}" alt="${article.title}">
+                                                        <figcaption>${article.title}</figcaption>
+                                                      </figure>`;
+    }
+    for (let i = 0; i < 3 && i < articles.length; i++) {
+      const article = articles[i];
+      const category = article.category;
+      categories.add(category);
+    }
+    for (let item of categories) {
+      filters.innerHTML += `<div class="filter ${item.id}">${item.name}</div>`;
+    }
+    const categoryFilters = document.querySelectorAll(".filter");
+    categoryFilters[0].classList.add("filter__selected");
+    categoryFilters.forEach((filter) => {
+      filter.addEventListener("click", () => {
+        const categoryId = parseInt(filter.classList[1]);
+        let filteredArticles;
+        if (categoryId === 0) {
+          filteredArticles = articles;
+        } else {
+          filteredArticles = articles.filter(
+            (article) => article.categoryId === categoryId
+          );
+        }
+        updateGallery(filteredArticles);
+      });
+    });
+  } catch (error) {
+    console.error(error);
   }
 }
-fetch('http://localhost:5678/api/works')
-  .then(r => 
-    { if(r.ok){r.json()
-  .then(jsonListArticle => {
-    for(let jsonArticle of jsonListArticle){
-    let article = new Article(jsonArticle)
-    document.querySelector('.gallery').innerHTML += `<figure>
-                                                          <img src="${article.imageUrl}" alt="${article.title}">
-                                                          <figcaption>${article.title}</figcaption>
-                                                      </figure>`
-    }
-  })
-  }})
+fetchWorks();
 
-fetch('http://localhost:5678/api/categories')
-  .then(r => r.json())
-  .then(jsonListCategories => {
-    for(let jsonCategorie of jsonListCategories){}
-  })
+function updateGallery(filteredArticles) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+  for (let article of filteredArticles) {
+    gallery.innerHTML += `<figure class="${article.categoryId}">
+                            <img src="${article.imageUrl}" alt="${article.title}">
+                            <figcaption>${article.title}</figcaption>
+                          </figure>`;
+  }
+}
